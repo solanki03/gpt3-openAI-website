4952bbda4e7e39078b50e4dc85df729121ea7461
solanki03 <solankisingha23@gmail.com>
2024-06-26 21:54:32 +0530

    Navigation bar for mobile view is created. Hero Section elements are added. Images are added.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 export default {
   content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontSize:{
+        esm:"0.75rem",
+        sm:"0.875rem",
+        base:"1rem",
+        md:"1.125rem",
+        lg:"1.25rem",
+        xl:"1.5rem",
+        "2xl":"2.125rem",
+        "3xl":"3.875rem"
+      }
+    },
   },
   plugins: [],
 }
